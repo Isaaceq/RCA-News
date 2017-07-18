12f494268267e7884698f337a62489af03f44561
Isaaceq <isaaceq25@gmail.com>
2017-07-17 19:04:46 -0700

    Add files via upload

--- services/arsTechnica.js
+++ services/arsTechnica.js
@@ -1,12 +1,10 @@
-app.service('arsTechnicaService', function($http) {
+app.service('arsTechnicaService', function ($http) {
 
-  this.getTopNews = function() {
-    return $http.get('_config.url');
+  this.getTopNews = function () {
+    return $http.get("https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=9240dca1a30e496abc175b508f2d633f");
   }
 
-  this.getLatestNews = function() {
-    return $http.get(' _config.url');
-    }
-
-
-  });
+  this.getLatestNews = function () {
+    return $http.get("https://newsapi.org/v1/articles?source=engadget&sortBy=latest&apiKey=9240dca1a30e496abc175b508f2d633f");
+  }
+});
