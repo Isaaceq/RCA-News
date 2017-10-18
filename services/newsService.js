7702d47a9a9247ff659790cc6754863b4a1c3f78
Isaaceq <isaaceq25@gmail.com>
2017-10-18 10:31:32 -0700

    API update

--- services/newsService.js
+++ services/newsService.js
@@ -1,11 +1,11 @@
 app.service('newsService', function ($http) {
 
   this.getLatestNews = function () {
-    return $http.get("https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=9240dca1a30e496abc175b508f2d633f");
+    return $http.get("https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=");
   }
 
   this.getTopNews = function () {
-    return $http.get("https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=9240dca1a30e496abc175b508f2d633f");
+    return $http.get("https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=");
   }
 
 });
